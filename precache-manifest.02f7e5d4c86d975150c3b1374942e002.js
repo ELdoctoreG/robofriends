4a75cb316f5736b8cca0468c851e58f0ec06e5e8
Gabriel Cruceanu <gabriel.cruceanu@softgalaxy.ro>
2019-11-05 13:03:12 +0200

    Updates

---
+++ precache-manifest.02f7e5d4c86d975150c3b1374942e002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4635e8282bac74a2f7718040b848efc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "19c907eb517d29774165",
+    "url": "/robofriends/static/css/main.424e8705.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "19c907eb517d29774165",
+    "url": "/robofriends/static/js/main.919d6624.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
